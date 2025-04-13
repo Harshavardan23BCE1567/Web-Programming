1203491010539dfe00b68263d20a90aebf3b575e
Harshavardan23BCE1567 <harshavardan.n2023@vitstudent.ac.in>
2025-04-13 20:56:36 +0530

    Delete Exercise-15 directory

--- Exercise-15/src/ParentComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ChildComponent from './ChildComponent';
-
-const ParentComponent = () => {
-  return <ChildComponent message="Hello from Parent!" />;
-};
-
-export default ParentComponent;
--- Exercise-15/src/StylButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-const StyledButton = () => {
-  return <button className="styled-button">Styled Button</button>;
-};
-
-export default StyledButton;
